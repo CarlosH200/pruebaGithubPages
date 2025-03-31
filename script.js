273a2e6a7f57c4a42f7734a4d728635a93caa523
CarlosH200 <carloshugop46@gmail.com>
2025-03-31 14:55:46 -0600

    primer cambio

---
+++ script.js
@@ -0,0 +1,5 @@
+    /* script.js */
+    document.getElementById("form-contacto").addEventListener("submit", function(event) {
+        event.preventDefault();
+        alert("Mensaje enviado correctamente!");
+    });
